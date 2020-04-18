1f5bde34488d7c5c9e3c925f8cb3591e0064ea05
Konstantin Tenekedzhiev <konstantin.tenekedzhiev@eldersoss.com>
2020-04-18 23:39:06 +0300

    Consul revision store

---
+++ src/Elders.Cronus.AtomicAction.Consul/RevisionStore/ConsulRevisionStore.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Elders.Cronus;
+using Elders.Cronus.Userfull;
+
+namespace Cronus.AtomicAction.Consul
+{
+    public class ConsulRevisionStore : IRevisionStore, IDisposable
+    {
+        private IConsulClient client;
+
+        public ConsulRevisionStore(IConsulClient client)
+        {
+            this.client = client;
+        }
+
+        public Result<int> GetRevision(IAggregateRootId aggregateRootId)
+        {
+            var key = GetRevisionKey(aggregateRootId);
+            var readKeyValues = client.ReadKeyValueAsync(key).Result;
+            var found = readKeyValues.FirstOrDefault(x => x.Key == key);
+            if (found is null)
+                return new Result<int>().WithError($"Revision for aggregate root '{aggregateRootId.Value}' was not found.");
+
+            var bytes = Convert.FromBase64String(found.Value);
+            var revision = int.Parse(Encoding.UTF8.GetString(bytes, 0, bytes.Length));
+
+            return new Result<int>(revision);
+        }
+
+        public Result<bool> HasRevision(IAggregateRootId aggregateRootId)
+        {
+            var key = GetRevisionKey(aggregateRootId);
+            var readKeyValues = client.ReadKeyValueAsync(key).Result;
+            var found = readKeyValues.Any(x => x.Key == key);
+
+            return new Result<bool>(found);
+        }
+
+        public Result<bool> SaveRevision(IAggregateRootId aggregateRootId, int revision, TimeSpan ttl)
+        {
+            var revisionKey = GetRevisionKey(aggregateRootId);
+            var sessionName = $"session/{revisionKey}-{revision}";
+            var session = client.CreateSessionAsync(new ConsulClient.CreateSessionRequest(sessionName, (int)ttl.TotalSeconds)).Result;
+            if (session.Success == false)
+                return new Result<bool>(false).WithError($"Unable to create session '{sessionName}', ttl '{ttl.TotalSeconds}'s");
+
+            var created = client.CreateKeyValueAsync(new ConsulClient.CreateKeyValueRequest(revisionKey, revision, session.Id)).Result;
+            return new Result<bool>(created);
+        }
+
+        public void Dispose()
+        {
+            (client as IDisposable)?.Dispose();
+            client = null;
+        }
+
+        private string GetRevisionKey(IAggregateRootId aggregateRootId) => $"revision/{Convert.ToBase64String(aggregateRootId.RawId)}";
+    }
+}
---
+++ src/Elders.Cronus.AtomicAction.Consul/RevisionStore/IRevisionStore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Elders.Cronus;
+using Elders.Cronus.Userfull;
+
+namespace Cronus.AtomicAction.Consul
+{
+    public interface IRevisionStore
+    {
+        Result<bool> HasRevision(IAggregateRootId aggregateRootId);
+
+        Result<int> GetRevision(IAggregateRootId aggregateRootId);
+
+        Result<bool> SaveRevision(IAggregateRootId aggregateRootId, int revision, TimeSpan ttl);
+    }
+}
